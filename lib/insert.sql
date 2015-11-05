f3446c1f9cbf235715464a4008d86860134149b8
NoneOfMaster <rcraigt@gmail.com>
2015-11-04 23:03:12 -0800

    funny bear pun

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 19, "M", "Brown", "Bad Boy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 17, "F", "Brown", "Foxy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 12, "F", "Brown", "Jovial", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 22, "M", "Brown", "Manic Depressive", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 41, "F", "Brown", "Not Cautious", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 99, "M", "Brown", "Grumpy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "M", "Brown", "Unspecific", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 78, "M", "Brown", "Misunderstood", 0);
